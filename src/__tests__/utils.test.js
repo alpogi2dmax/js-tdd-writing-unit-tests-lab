0bfd7787b803ef0b933fcbcff7526d419580ec65
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-08 11:26:37 -0800

    complete js tdd writing unit tests lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,32 @@
 // Your tests here
+import { isPalindrome } from '../utils'
+
+describe('isPalindrome', () => {
+    it('confirms if "mom" is a palindrome', () => {
+        const word = 'mom';
+        const confirm = isPalindrome(word);
+        expect(confirm).toBe(true);
+    });
+
+    it('confirms if "RaceCar" is a palindrome regardless of case', () => {
+        const word = 'RaceCar';
+        const confirm = isPalindrome(word);
+        expect(confirm).toBe(true);
+    });
+
+    it('returns false for an empty string', () => {
+        const word = '';
+        const confirm = isPalindrome(word);
+        expect(confirm).toBe(false);
+    });
+
+    it('throws an error for non-alphabetic characters', () => {
+        const word = 'racecar123';
+        expect(() => isPalindrome(word)).toThrow('Input must only contain alphabetic characters');
+    });
+
+    it('throws an error for non-string inputs', () => {
+        const word = 12345;
+        expect(() => isPalindrome(word)).toThrow('Input must be a string');
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
 // Your code here
+export function isPalindrome(word) {
+
+    if (word === '') {
+        return false
+    }
+    if (typeof word !== 'string') {
+        throw new Error('Input must be a string');
+    }
+
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error('Input must only contain alphabetic characters');
+    }
+    let reverseWord = word.toLowerCase().split('').reverse().join('');
+    return word.toLowerCase() === reverseWord;
+}
